cc450646b7e7d3bdd0a0529196891b582fb2f5cf
Chris Bryant <cbryant@caxy.com>
2024-04-22 13:34:14 -0400

    Added square site link to Nav.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,13 +6,15 @@ export default function Nav() {
     <Stack direction="row" spacing={2} sx={{
       '.MuiButtonBase-root': {
         fontSize: {
-          xs: '1.5rem',
-          sm: '2rem',
+          xs: '1rem',
+          sm: '1.5rem',
+          md: '2rem',
         }
       }
     }}>
       <Button size="large" href="/about">Our Story</Button>
       <Button size="large" href="/ontap">On Tap</Button>
+      <Button size="large" href="https://elderbrew.square.site/">Merch</Button>
       <Button size="large" href="/contact">Contact</Button>
     </Stack>
   );
